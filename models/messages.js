63021cbf83c905168552abb39a7b43cf5f809e54
maciej <maciejsz0@o2.pl>
2022-01-06 20:38:23 +0100

    messages routes

---
+++ models/messages.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  userId: {
+    type: String
+  },
+  message: {
+    type: String,
+    required: true
+  }, {
+    timestamps: true
+  }
+})
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
---
+++ routes/messageRoute.js
@@ -0,0 +1,24 @@
+const Message = require('../models/messages');
+const router = require('express').Router();
+const urlencodedParser = require('./urlEncoded');//to post requests
+
+router.route('/').get( (req, res) => {
+  Message.find()
+    .then( messages => res.json(messages))
+    .catch( err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/add').post(urlencodedParser, (req, res) => {
+
+  const userId = req.body.userId;
+  const message = req.body.message;
+
+  const newMessage = new Message({ userId, message });
+
+  newMessage.save()
+    .then( () => res.json('User added'))
+    .catch( err => res.send(err))
+  )
+})
+
+module.exports = router;
