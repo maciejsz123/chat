136fc5dc912d6cc346d9408be75241798de57b8f
maciej <maciejsz0@o2.pl>
2022-02-22 20:24:02 +0100

    save store after refresh

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,14 +1,17 @@
 import './App.sass';
 import MainPage from './components/mainPage/mainPage';
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="app">
-        <MainPage />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="app">
+          <MainPage />
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- chat/src/redux/store/index.js
+++ chat/src/redux/store/index.js
@@ -1,13 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const initialStore = {
-  
+
 };
+
+const persistConfig = {
+  key: 'root',
+  storage
+};
+const pReducer = persistReducer(persistConfig, rootReducer);
 
 let middleware = [thunk];
 
-const store = createStore(rootReducer, initialStore, applyMiddleware(...middleware));
+const store = createStore(pReducer, initialStore, applyMiddleware(...middleware));
 
-export default store;
+const persistor = persistStore(store);
+export { persistor, store };
