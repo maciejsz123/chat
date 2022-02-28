34d17200958b64d13d6a0dfa895d0f3fdc85c20e
maciej <maciejsz0@o2.pl>
2022-02-28 17:12:48 +0100

    commit

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve(__dirname, "./chat/build")));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.reslove(__dirname, 'chat', 'build', 'index.html'))
+    res.sendFile(path.reslove(__dirname, './client/build', 'index.html'))
   })
 }
 
